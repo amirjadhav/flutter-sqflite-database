3c20fd396ff8a3c5d88488185d0eca160e1819cb
Amir Jadhav <jadhav.amir100@gmail.com>
2020-09-30 18:56:49 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      title: "Flutter Database",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
